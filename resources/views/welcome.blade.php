6f35018e12748cb61a02f490a925a7ab87ef1370
TakanoriShima <quark2galaxy@gmail.com>
2022-06-08 14:46:37 +0900

    welcome.blade.php修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
         <img src="{{ ('/images/network1.jpg') }}" alt="最初に表示されるtop画面">
     </div>
     <div class="row mt-5">
-        {!! link_to_route('signup.get', '新規メンバー登録', [], ['class' => 'offset-sm-4 col-sm-4 btn btn-primary']) !!}
+        {!! link_to_route('signup.get', '新規職員登録', [], ['class' => 'offset-sm-4 col-sm-4 btn btn-primary']) !!}
     </div>
     <div class="row mt-3">
         {!! link_to_route('login', 'ログイン画面', [], ['class' => 'offset-sm-4 col-sm-4 mt-2 btn btn-danger']) !!}
